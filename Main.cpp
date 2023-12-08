1ef58801d9723933245d151e0f7a54109f121ba1
Asher Cronin <46426339+albgje@users.noreply.github.com>
2023-12-07 23:39:55 -0500

    cube!

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,15 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+#include<glm/glm/glm.hpp>
+#include<glm/glm/gtc/matrix_transform.hpp>
+#include<glm/glm/gtc/type_ptr.hpp>
+
 #include "shader_s.h";
 
+
+const unsigned int WIDTH = 800;
+const unsigned int HEIGHT = 800;
 
 void processInput(GLFWwindow* window)
 {
@@ -25,7 +32,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create a GLFWindow object of 800 by 800 named "OpenGL First Window"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL First Window", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL First Window", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -40,74 +47,137 @@ int main()
 	// Load GLAD so it configures OpenGL
 	gladLoadGL();
 
+	// Enable Depth Testing
+	glEnable(GL_DEPTH_TEST);
+
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viwport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, WIDTH, HEIGHT);
 
 	Shader shader("shader.vs", "shader.fs");
 
 	// Vertices coordinates
 	float vertices[] = {
 		// positions         // colors
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+		 0.5f - 0.25f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f - 0.25f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f - 0.25f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
 	};
 
-	/**
-	float vertices2[] =
-	{
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f, // top left 
-		-1.0f, -1.0f, 0.0f  //other
+	float vertices2[] = {
+		// positions         // colors
+		0.5f,   0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // front top right
+		0.5f,  -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // front bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f, // front bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f, // front top left 
 	};
 
 	float vertices3[] = {
-		// first triangle
-		-0.9f, -0.5f, 0.0f,  // left 
-		-0.0f, -0.5f, 0.0f,  // right
-		-0.45f, 0.5f, 0.0f,  // top 
-		// second triangle
-		 0.0f, -0.5f, 0.0f,  // left
-		 0.9f, -0.5f, 0.0f,  // right
-		 0.45f, 0.5f, 0.0f   // top 
+		// positions          // colors
+		0.5f,  0.5f, -1.0f,   1.0f, 1.0f, 0.0f, // right top right
+		0.5f, -0.5f, -1.0f,   1.0f, 0.0f, 1.0f, // right bottom right
+		0.5f,  -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, // right bottom left
+		0.5f,   0.5f, 0.0f,   1.0f, 0.0f, 0.0f, // right top left
 	};
 
 	unsigned int indices[] =
 	{
-		0, 1, 3, // 0, 1, 3
-		1, 2, 3  // 1, 2, 4
-	}; **/
+		0, 1, 3,
+		1, 2, 3,
+	};
+
+	float vertices10[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+
+		-0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+	};
 
 	// Create reference containers for the Vertex Array Object and the Vertex Buffer Object
-	unsigned int VAO, VBO; //, EBO;
-
-	// Always make sure to make a VAO before a VBO
-	// Generate the VAO and VBO with only 1 object each
-	glGenVertexArrays(1, &VAO); 
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	//glGenBuffers(1, &EBO);
 
-	// Bind VAO to make it the current VAO
 	glBindVertexArray(VAO);
-
-	// Bind VBO and specify that it is a GL_ARRAY_BUFFER
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	// Introduce the vertices into the VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices10), vertices10, GL_STATIC_DRAW);
 
-
-	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	// Enable the Vertex Attribute so that OpenGL knows ot use it
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);	// Vertex attributes stay the same
 	glEnableVertexAttribArray(0);
 
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	/*
+	unsigned int VAOs[2], VBOs[2], EBOs[2];
+
+	// Always make sure to make a VAO before a VBO
+	// Generate the VAO and VBO with 2 objects each
+	glGenVertexArrays(2, VAOs); 
+	glGenBuffers(2, VBOs);
+	glGenBuffers(2, EBOs);
+
+	
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);	// Vertex attributes stay the same
+	glEnableVertexAttribArray(0);
+	
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices3), vertices3, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);	// Vertex attributes stay the same
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	*/
+
 
 	// Bind both the VBO and VAO to 0 so that we don't accidentally modify them
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -130,18 +200,29 @@ int main()
 
 		// Set BG color
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// transformations
+		glm::mat4 transform = glm::mat4(1.0f);
+		transform = glm::rotate(transform, (float)glfwGetTime()*3.0f, glm::vec3(0.0f, 2.0f, 1.0f));
+
 		// Tell OpenGL which Shader Program we want to use
 		//glUseProgram(shaderProgram);
 		shader.use();
+		unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
 		// Bind the VAO so OpenGL knows to use it
 		glBindVertexArray(VAO);
-		// Draw the triangle using the GL_TRIANGLES primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// Draw rectangle
-		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		//glBindVertexArray(0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		/*
+		glBindVertexArray(VAOs[0]);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glBindVertexArray(VAOs[1]);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); 
+		*/
+
 
 		glfwSwapBuffers(window);
 
@@ -152,7 +233,11 @@ int main()
 	// Delete all the objects
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	//glDeleteBuffers(1, &EBO);
+	/*
+	glDeleteVertexArrays(2, VAOs);
+	glDeleteBuffers(2, VBOs);
+	glDeleteBuffers(2, EBOs);
+	*/
 	//glDeleteProgram(shaderProgram);
 
 	// Delete window before ending the program
